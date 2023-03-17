5c4c95b3c7b6ad33952ee384e6e7d0c074bda1b0
Ivan Molto <ivanmolto@gmail.com>
2023-03-18 00:11:16 +0100

    chore: Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ApeCoin from "./routes/ape-coin";
+import Governance from "./routes/governance";
+import Treasury from "./routes/treasury";
+import Staking from "./routes/staking";
+import WalletProfiler from "./routes/wallet-profiler";
+import Collections from "./routes/collections";
+import ErrorPage from "./error-page";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "apecoin",
+        element: <ApeCoin />,
+      },
+      {
+        path: "governance",
+        element: <Governance />,
+      },
+      {
+        path: "treasury",
+        element: <Treasury />,
+      },
+      {
+        path: "staking",
+        element: <Staking />,
+      },
+      {
+        path: "walletprofiler",
+        element: <WalletProfiler />,
+      },
+      {
+        path: "collections",
+        element: <Collections />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
