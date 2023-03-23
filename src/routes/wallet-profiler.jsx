f39b75fa5a694b0f42f7a385db4ce748fbba90f2
Ivan Molto <ivanmolto@gmail.com>
2023-03-23 23:34:37 +0100

    chore: Add Wallet Profiler initial route UI

--- src/routes/wallet-profiler.jsx
+++ src/routes/wallet-profiler.jsx
@@ -1,5 +1,12 @@
+import VotingPower from "../../components/voting-power";
+
 const WalletProfiler = () => {
-  return <div>Wallet Profiler</div>;
+  return (
+    <>
+      <div>Wallet Profiler</div>
+      <VotingPower />
+    </>
+  );
 };
 
 export default WalletProfiler;
